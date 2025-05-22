482428ee1d045d9f7ba93a7a1b461efce15aac6d
JoaoPedro-MAAR <2005joaopedromaar@gmail.com>
2025-05-22 19:57:21 -0300

    Acho que ta finalizado

--- EnjoeiCarro/src/Operacoes/Alterar.java
+++ EnjoeiCarro/src/Operacoes/Alterar.java
@@ -6,7 +6,6 @@ import com.db4o.ObjectContainer;
 import com.db4o.query.Query;
 
 import Classes.Carro;
-import Classes.Fabricante;
 import Classes.Modelo;
 
 public class Alterar {
@@ -14,25 +13,48 @@ public class Alterar {
 
 	public static void main(String[] args) {
 		manager = Util.conectarBanco();
-		System.out.println(
-				"Alteracao 1: Adicionar um relacionamento inexistente entre objetos. Adicionar modelo Novo a chevrolet e excluir um modelo antigo de chevrolet");
 
-		Query q = manager.query();
-		q.constrain(Fabricante.class);
-		q.descend("nome").constrain("Chevrolet");
-		List<Fabricante> resultados = q.execute();
-		if(resultados.size()>0) {
-			System.out.println("OI");
-			Fabricante chev = resultados.getFirst();
-			chev.addModelo(new Modelo("Novo", chev));
-			chev.rmvModelo(chev.getModelos().getFirst());
-			manager.store(chev);
+		
+		System.out.println("Alteracao: Criar um novo carro e adicionar em um modelo e apagar um carro do mesmo modelo");
+		Query q1 = manager.query();
+		q1.constrain(Modelo.class);
+		q1.descend("nome").constrain("Onix");
+		List<Modelo> modelos = q1.execute();
+		if(modelos.size()>0) { 
+			Modelo onix = modelos.getFirst();
+			System.out.println("\nOnix antes de adicionar novo carro: \n"+onix);
+			Carro dpp = new Carro("DPP2222", 2018, 120000.0, onix);
+			System.out.println("\nCarro que vai ser adicionado: "+dpp);
+			System.out.println("\nOnix depois de adicionar novo carro: \n"+onix);
+			
+			List<Carro> carrosOnix = onix.getLista_de_carros();
+			if(carrosOnix.size()>0) {
+				System.out.println("\nOnix antes de deletar um carro: \n"+onix);
+				Carro carroDeletado = carrosOnix.get(0);
+				onix.rmvCarro(carroDeletado);
+				System.out.println("\nOnix depois de deletar um carro: \n"+onix);
+
+				manager.delete(carroDeletado);
+			}
+			
+			manager.store(onix);
 			manager.commit();
+			
+			
+			
 		}
 		
-
+		
+		
+		
 		Util.desconectar();
-		System.out.println("fim...");
+		System.out.println("\nFim do Alterar");
+		
+		
+		
+		
+		
+		
 	}
 
 }
